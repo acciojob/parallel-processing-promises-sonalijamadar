da909a70d9bbb8f270467b9ed31d229697afec48
Sonali Jamadar <99808291+sonalijamadar@users.noreply.github.com>
2026-01-08 17:15:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //your JS code here. If required.
 const output = document.getElementById("output");
 const btn = document.getElementById("download-images-button");
+button.id = "download-images-button";
+button.textContent = "Download Images";
 
 const images = [
   { url: "https://picsum.photos/id/237/200/300" },
@@ -15,8 +17,9 @@ loading.textContent = "Loading...";
 const errorDiv = document.createElement("div");
 errorDiv.id = "error";
 
+document.body.insertBefore(button, output);
 document.body.appendChild(loading);
-document.body.appendChild(errorDiv)
+document.body.appendChild(errorDiv);
 
 function downloadImage(url) {
   return new Promise((resolve, reject) => {
@@ -28,6 +31,7 @@ function downloadImage(url) {
   });
 }
 function downloadImages() {
+  output.innerHTML = "";
   loading.style.display = "block";
   errorDiv.textContent = "";
 
